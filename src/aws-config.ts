e7af8b4a6b4be727dab98de9b320715fe31de356
Brey <breywarren1@gmail.com>
2026-02-18 21:49:53 -0500

    Fix TypeScript config type

--- src/aws-config.ts
+++ src/aws-config.ts
@@ -3,7 +3,7 @@ export const awsConfig = {
     Cognito: {
       userPoolId: 'us-east-2_p4ktzlX5I',
       userPoolClientId: '7tbf76a2nmsulp3g0lk1vave4g',
-      signUpVerificationMethod: 'code',
+      signUpVerificationMethod: 'code' as const,
     }
   }
 };
